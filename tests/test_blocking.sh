1bc0429c662b65bf2761e242e15cbeb9e4dc68ba
Dan Aimone <30274286+danaimone@users.noreply.github.com>
2020-05-15 16:28:36 -0700

    added blocking test script

---
+++ tests/test_blocking.sh
@@ -0,0 +1,20 @@
+#!/usr/pkg/bin/bash
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+printf "${bold}Executing telnet for quote of the day.. ${normal}\n"
+telnet fd10:2020:c5c1:367:e15f:7bc8:97a3:cf9d 13
+
+printf "\n"
+printf "${bold}Executing telnet for time...${normal}\n"
+telnet fd10:2020:c5c1:367:e15f:7bc8:97a3:cf9d 17
+
+printf "\n"
+printf "${bold}Executing netstat...${normal}\n"
+netstat -nf inet6
+
+printf "\n"
+printf "${bold}Executing netstat with -a flag...${normal}\n"
+netstat -anf inet6
+printf "\n"
+
